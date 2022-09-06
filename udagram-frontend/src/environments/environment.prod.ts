981b264a5a86241bbdd07757ba9904f396e21d05
oadeniran <oadeniran82@gmail.com>
2022-09-06 06:41:03 +0100

    final-files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a978117ea2285408e837eaaf40477373-588241339.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
